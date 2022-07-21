e8ac2798ce77b9480835e4186105037b3a8d41f1
Diego Solis <disc3110@gmail.com>
2022-07-20 22:19:15 -0700

    create player and game objects

---
+++ script.js
@@ -0,0 +1,10 @@
+const Player = (name, symbol) => {
+ const getName = () => name;
+ const getSymbol = () => symbol;
+
+ return {getName, getSymbol}
+}
+
+const TicTacToe = (player1, player2) => {
+  let currentPlayer = player1
+}
