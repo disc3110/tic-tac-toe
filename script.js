c9dc1777cd0269e03cfb577413e0297d31699c17
Diego Solis <disc3110@gmail.com>
2022-12-28 13:21:27 -0800

    add feature to save players

--- script.js
+++ script.js
@@ -1,34 +1,108 @@
-let gameZone = document.getElementById('game');
+const Screen = (() => {
 
-const Board = () => {
+  let gameZone = document.getElementById('game');
 
-  function markcell(){
+  const clearZone = () => gameZone.innerHTML = ''
+
+  function markcell(square){
+    console.log(square.target.id)
     this.style.backgroundColor='black';
   }
 
- let createGrid = () => {
-  for (let i=1; i <= 9 ; i++){
-    newBox = document.createElement('div');
-    newBox.classList.add('boxes');
-    newBox.classList.add(`box-${i}`);
-    newBox.style.borderWidth = "0.5px";
-    newBox.style.borderStyle = "solid";
-    newBox.addEventListener('click',markcell);
-    gameZone.appendChild(newBox)
+  let askPlayerInfo = () => {
+    clearZone()
+    let playerInfoZone 
+    playerInfoZone = document.createElement('div')
+    playerInfoZone.innerHTML = 
+    '<form> <labe class="form-label"> Whats the name of the first player? </label> <input id="player-name" type="text" class="form-control"> <br> <label class="form-label"> Whats the color of the first player? </label> <input id="player-color" type="color" class="form-control"> <br> <p id="get-info-btn" class="btn btn-primary"> Submit </p > </form>'
+    gameZone.appendChild(playerInfoZone)
+    const getInfoBtn = document.getElementById('get-info-btn')
+    getInfoBtn.addEventListener('click', TicTacToe.getPlayers)
+    console.log(getInfoBtn)
   }
+
+  let createGrid = () => {
+    clearZone()
+    let newBox
+    for (let i=1; i <= 9 ; i++){
+      newBox = document.createElement('div');
+      newBox.classList.add('boxes');
+      newBox.setAttribute('id', `${i}`)
+      newBox.classList.add(`box-${i}`);
+      newBox.style.borderWidth = "0.5px";
+      newBox.style.borderStyle = "solid";
+      newBox.addEventListener('click',markcell, false);
+      gameZone.appendChild(newBox)
+    }
  }
 
- return {createGrid}
+ return {createGrid, askPlayerInfo, clearZone}
+})()
+
+
+const Player = (name, color) => {
+  let score = 0
+
+  const incrementScore = () =>  score++ 
+
+  const getScore = () => score
+
+  return { name , color, incrementScore, getScore}
 }
 
-const TicTacToe = (player1, player2) => {
+const TicTacToe = (() => {
+  let player1, player2
+
+  let savePlayerInfo = () => {
+    let playerName = document.getElementById('player-name').value;
+    let playerColor = document.getElementById('player-color').value;
+    Screen.clearZone()
+    return {playerName, playerColor}
+  }
+
+  const getPlayers = () => {
+    if (!player1) {
+      let playerInfo = savePlayerInfo()
+      player1 = Player(playerInfo.playerName, playerInfo.playerColor)
+      console.log(playerInfo, player1)
+      Screen.askPlayerInfo()
+    } else if (!player2) {
+      let playerInfo = savePlayerInfo()
+      player2 = Player(playerInfo.playerName, playerInfo.playerColor)
+      console.log(playerInfo, player1, player2)
+      game()
+    } 
+  }
+
+  const game = () => {
+    let currentplayer = player1
+    Screen.createGrid()
+  }
+
+  const init = () => {
+    Screen.askPlayerInfo()
+  }
+
+  return {
+    init, getPlayers
+  }
+})()
+
+TicTacToe.init()
+
+
+/*
+const TicTacToe = ((player1, player2) => {
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
   let currentplayer = player1
+  let gameFinished = false
+  let winner = null
 
   let rotateCurrentPlayer = () => {
     currentplayer = currentplayer == player1 ? player2 : player1
   }
-}
 
-const newGame = Board();
-newGame.createGrid()
+  return {currentplayer}
+})()
+
+*/
